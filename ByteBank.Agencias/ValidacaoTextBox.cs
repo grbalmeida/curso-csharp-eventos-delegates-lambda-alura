544c764c888a74fa688168039689bdda714405e5
gilvan <g.r.almeida@live.com>
2020-09-06 20:53:32 -0300

    Executando nossos eventos

--- ByteBank.Agencias/ValidacaoTextBox.cs
+++ ByteBank.Agencias/ValidacaoTextBox.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ByteBank.Agencias
 {
@@ -7,5 +8,22 @@ namespace ByteBank.Agencias
     public class ValidacaoTextBox : TextBox
     {
         public event ValidacaoEventHandler Validacao;
+
+        public ValidacaoTextBox()
+        {
+            TextChanged += ValidacaoTextBox_TextChanged;
+        }
+
+        private void ValidacaoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Validacao != null)
+            {
+                var ehValido = Validacao(Text);
+
+                Background = ehValido
+                    ? new SolidColorBrush(Colors.White)
+                    : new SolidColorBrush(Colors.OrangeRed);
+            }
+        }
     }
 }
